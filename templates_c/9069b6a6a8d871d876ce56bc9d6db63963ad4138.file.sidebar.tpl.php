508544e712f001df81a02afbef3be6e198f34193
skjun <skjun.java@gmail.com>
2013-08-27 23:38:16 +0800

    add

--- templates_c/9069b6a6a8d871d876ce56bc9d6db63963ad4138.file.sidebar.tpl.php
+++ templates_c/9069b6a6a8d871d876ce56bc9d6db63963ad4138.file.sidebar.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.13, created on 2013-08-26 17:29:00
+<?php /* Smarty version Smarty-3.1.13, created on 2013-08-27 15:50:41
          compiled from "E:\phpweb\tickets\templates\admin\sidebar.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:2457652198277be31c6-13960448%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '9069b6a6a8d871d876ce56bc9d6db63963ad4138' => 
     array (
       0 => 'E:\\phpweb\\tickets\\templates\\admin\\sidebar.tpl',
-      1 => 1377530900,
+      1 => 1377610713,
       2 => 'file',
     ),
   ),
@@ -21,17 +21,22 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_52198277bf9e07_50118957')) {function content_52198277bf9e07_50118957($_smarty_tpl) {?><div class="sidebar-nav"> 
 		
-	       <a href="#sidebar_menu_1" class="nav-header collapsed" data-toggle="collapse"><i class="icon-th"></i>系统展示管理<i class="icon-chevron-up"></i></a>
+	       <a href="#sidebar_menu_1" class="nav-header collapsed" data-toggle="collapse"><i class="icon-th"></i>票务信息管理<i class="icon-chevron-up"></i></a>
 		 
 				<ul id="sidebar_menu_1" class="nav nav-list collapse in">
 					<li><a href="<?php echo @constant('WEBSITE_URL');?>
-admin/param/load/paramtype=rights">会员权益更新</a></li>
+admin/ticket">票务信息查看</a></li>
 					<li><a href="<?php echo @constant('WEBSITE_URL');?>
-admin/param/load/paramtype=faq">常见问题更新</a></li>
+admin/ticket">票务信息总体统计</a></li>
+					 
+				</ul> 
+	   <a href="#sidebar_menu_1" class="nav-header collapsed" data-toggle="collapse"><i class="icon-th"></i>用户信息管理<i class="icon-chevron-up"></i></a>
+		 
+				<ul id="sidebar_menu_1" class="nav nav-list collapse in">
 					<li><a href="<?php echo @constant('WEBSITE_URL');?>
-admin/param/load/paramtype=google">GOOGLE统计更新</a></li>
+admin/adminuser">管理员用户</a></li>
 					<li><a href="<?php echo @constant('WEBSITE_URL');?>
-admin/activities">会员专享活动管理</a></li>
+admin/users">前台注册用户</a></li> 
 					<li><a href="<?php echo @constant('WEBSITE_URL');?>
 admin/userLog">用户日志</a></li>
 				</ul> 
@@ -39,9 +44,9 @@ admin/userLog">用户日志</a></li>
 		 
 				<ul id="sidebar_menu_1" class="nav nav-list collapse in">
 					<li><a href="<?php echo @constant('WEBSITE_URL');?>
-admin/user">管理员管理</a></li> 
+admin/user">系统参数管理</a></li> 
 					<li><a href="<?php echo @constant('WEBSITE_URL');?>
-admin/userLog">用户日志</a></li>
+admin/userLog">其他功能</a></li>
 			 </ul> 
         <a target="_blank" href="#" class="nav-header" ><i class="icon-question-sign"></i>帮助</a>
 </div> 
