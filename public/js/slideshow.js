d6d740e62ade32421541e68e31dcf72b3768e3b3
superyuyue <superyuyue@126.com>
2013-11-03 15:57:07 +0800

    aa

--- public/js/slideshow.js
+++ public/js/slideshow.js
@@ -30,33 +30,33 @@
 }
 ;
 function SlideShow2(c) {
-    var a = document.getElementById("slideContainer2"), f = document.getElementById("slidesImgs2").getElementsByTagName("li"), h = document.getElementById("slideBar2"), n = h.getElementsByTagName("li"), d = f.length, c = c || 3000, e = lastI = 0, j, m;
-    function b() {
-        m = setInterval(function () {
-            e = e + 1 >= d ? e + 1 - d : e + 1;
-            g()
-        }, c)
+    var a2 = document.getElementById("slideContainer2"), f2 = document.getElementById("slidesImgs2").getElementsByTagName("li"), h2 = document.getElementById("slideBar2"), n2 = h.getElementsByTagName("li"), d2 = f.length, c2 = c2 || 3000, e2 = lastI2 = 0, j2, m2;
+    function b2() {
+        m2 = setInterval(function () {
+            e2 = e2 + 1 >= d2 ? e2 + 1 - d2 : e2 + 1;
+            g2()
+        }, c2)
     }
-    function k() {
-        clearInterval(m)
+    function k2() {
+        clearInterval(m2)
     }
-    function g() {
-        f[lastI].style.display = "none";
-        n[lastI].className = "";
-        f[e].style.display = "block";
-        n[e].className = "on";
-        lastI = e
+    function g2() {
+        f[lastI2].style.display = "none";
+        n[lastI2].className = "";
+        f[e2].style.display = "block";
+        n[e2].className = "on";
+        lastI2 = e2
     }
-    f[e].style.display = "block";
-    a.onmouseover = k;
-    a.onmouseout = b;
-    h.onmouseover = function (i) {
-        j = i ? i.target : window.event.srcElement;
-        if (j.nodeName === "LI") {
-            e = parseInt(j.innerHTML, 10) - 1;
-            g()
+    f[e2].style.display = "block";
+    a2.onmouseover = k2;
+    a2.onmouseout = b2;
+    h2.onmouseover = function (i) {
+        j2 = i ? i.target : window.event.srcElement;
+        if (j2.nodeName === "LI") {
+            e2 = parseInt(j2.innerHTML, 10) - 1;
+            g2()
         }
     };
-    b()
+    b2()
 }
 ;
